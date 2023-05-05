c26349cae9f5bfe04de77ffab1bc808695de094f
Harrison Bowlby <hrbowlby@gmail.com>
2023-05-05 15:22:52 -0700

    initial commit -fixed nan bug

---
+++ README.md
@@ -0,0 +1,13 @@
+## Excel totals reader
+## Owner Harrison Bowlby
+
+This is a tool I created for the company Sollid Cabinetry that helps in determining the total counts of certain inventory stock.
+
+## Installation
+If you do not have it installed already or are not using Anaconda you will need to install pandas. "pip install pandas" And you'll need python installed if you already do not have it.
+
+## Usage
+At the moment the code only runs when you replace the sheet location for the excelReader function on line 38. In the future I plan to add a GUI that allows you to input the location from anywhere.
+
+## Contributing
+Feel free to grab this code and use it for your own
---
+++ main.py
@@ -0,0 +1,42 @@
+import pandas as pd
+
+
+def excelReader(sheet):
+    # Read the excel file and save each sheet
+    spreadsheet = pd.ExcelFile(sheet)
+    modelAndQuanityDict = {}
+
+    for sheet_name in spreadsheet.sheet_names:
+        # Read each sheet and save it as a dataframe
+        df = pd.read_excel(spreadsheet, sheet_name=sheet_name)
+
+        # Get the index for "Item Number":
+        item_number_index = df.columns.get_loc("Item Number")
+
+        for row_index, row in df.iterrows():
+            # Get the model name from the "Item name" column
+            model_name = row.iloc[item_number_index]
+
+            # Check if the model name already exists in dictionary
+            if model_name not in modelAndQuanityDict:
+                # Add it and then set the quantity to 0
+                modelAndQuanityDict[model_name] = {"total": 0}
+
+            # Loop through the columns and find the counts
+            for col_name, cell in row.items():
+                # Check the column name that would contain count
+                if (
+                    col_name == "Available"
+                    or col_name == "Trans Count Qty"
+                    or col_name == "Trans Count"
+                ):
+                    modelAndQuanityDict[model_name]["total"] += cell
+
+    return modelAndQuanityDict
+
+# convert the returned data to excel
+totals = excelReader("spreadsheets/allCountsForProgram.xlsx")
+
+df = pd.DataFrame.from_dict(totals, orient="index")
+
+df.to_excel("newTotals.xlsx", index_label="Item_number")
